b8bab8eff8d3df6453dda37fb4dc5f8b774f98cb
philles85 <philster04@outlook.com>
2026-05-06 08:53:57 +0200

    Folder structure

---
+++ client/views/landingView/LandingView.js
@@ -0,0 +1,16 @@
+class LandingView {
+
+    constructor() {
+
+    }
+
+
+
+
+    render() {
+
+    }
+
+
+
+}
---
+++ client/views/landingView/components/TerminalPrompt.js
@@ -0,0 +1,20 @@
+class TerminalPrompt extends HTMLElement {
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+
+
+
+    render() {
+
+
+
+    }
+
+
+
+}
+
+customElements.define("terminal-prompt", TerminalPrompt)
