d9e0f115394e5d47b6b7ef1b51ce0aae1e556844
philles85 <philster04@outlook.com>
2026-05-13 23:32:56 +0200

    AService: Testing class for getting highest score for participants

--- client/views/AView/AService.js
+++ client/views/AView/AService.js
@@ -34,6 +34,88 @@ for (let i = 0; i < compDays.length; i++) {
 console.log(totalParticipants);
 console.log(Object.keys(totalParticipants).length)
 
+
+
+class AService {
+
+    constructor(discipline, season, locationId) {
+        this.DB = DB;
+        this.chosenDicsiplineName = discipline;
+        this.chosenSeasonNum = season;
+        this.locationId = locationId;
+        this.totalScore = [];
+        this.getAllScore();
+    }
+
+    getSeason() {
+        if (!this.chosenSeasonNum) {
+            return DB.seasons;
+        } else {
+            let filterdSeason = this.DB.seasons.find(season => season.year == this.chosenSeasonNum);
+            return filterdSeason;
+        }
+    }
+
+    getDiscipline() {
+        if (!this.chosenDicsiplineName) {
+            return DB.disciplines;
+        } else {
+            let filteredDiscipline = this.DB.disciplines.find(discipline => discipline.name == this.chosenDicsiplineName);
+
+            return filteredDiscipline;
+        }
+    }
+
+    getAllScore() {
+
+        for (let compDay of this.getSeason()["competitionDays"]) {
+
+            if (this.locationId == compDay.locationId) {
+
+                for (let event of compDay.events) {
+
+                    // Denna kollar bara om en det är en disciplines, men inte om det är alla disciplines, kollar över det sen
+                    if (event.disciplineId == this.getDiscipline().id) {
+
+                        for (let participant of event.scores) {
+
+                            if (!this.totalScore.some(partici => partici.id == participant.participantId)) {
+
+                                console.log("hehjj")
+
+                                this.totalScore.push({ id: participant.participantId, score: participant.score });
+
+                            } else {
+
+                                for (let partici of this.totalScore) {
+
+                                    if (partici.id == participant.participantId) {
+
+                                        partici.score += participant.score;
+
+                                    }
+
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        console.log(this.totalScore);
+    }
+
+
+
+}
+
+
+
+new AService("Debugging", 1, 3);
+
+
+
 // if (totalParticipants.some(object => object.participantId != participant.participantId)) {
 //     totalParticipants.push({ participantId: participant.participantId, participatedTimes: 1 });
 // } 
