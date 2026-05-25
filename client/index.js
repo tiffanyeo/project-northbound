93651c9d9ae2b61e9427696f55591217a3457a29
sjfalina <alinab91@hotmail.com>
2026-05-25 11:14:17 +0200

    Add C-view

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import "./views/countryLandingView/countryLandingView.js";
 import "./views/AView/AView.js";
 
 import { BView } from"./views/B-View/BView.js"
+import { CView } from"./views/CView/CView.js"
 
 window.addEventListener("selected-agent", (data) =>{
     const app = document.querySelector("#app");
@@ -11,6 +12,11 @@ window.addEventListener("selected-agent", (data) =>{
 
 })
 
+window.addEventListener("selected-country", (data) =>{
+    const app = document.querySelector("#app");
+    app.appendChild(new CView(data.detail.id));
+
+})
 
 //TestView för BarChart just nu.
 //import "./views/test/testView.js"
